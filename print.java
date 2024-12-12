cad42a486fc7a3ecc3d99d86b530aaceae7d8c6e
Arul-252005 <oparul16@gmail.com>
2024-12-12 13:26:59 +0530

    Update print.java

--- print.java
+++ print.java
@@ -2,6 +2,6 @@ class print
 	{
 		public static void main(String args[])
 			{
-				System.out.println("It is used to create a repo in git");
+				System.out.println("It is used to create a repository in github");
 			}
 	}
